b69ac0e1ad2b2801f023c38581862dcbcc089caf
PEPETI BALAJI <pepeti.balaji@oracle.com>
2026-06-03 23:58:55 +0530

    feat(inventory): implement stock reservation logic

---
+++ inventory-service/src/main/java/com/ecommerce/inventory/service/InventoryService.java
@@ -0,0 +1,76 @@
+package com.ecommerce.inventory.service;
+
+import com.ecommerce.common.exception.ResourceNotFoundException;
+
+import com.ecommerce.inventory.dto.InventoryResponse;
+
+import com.ecommerce.inventory.entity.Inventory;
+
+import com.ecommerce.inventory.mapper.InventoryMapper;
+
+import com.ecommerce.inventory.repository.InventoryRepository;
+
+import lombok.RequiredArgsConstructor;
+
+import org.springframework.stereotype.Service;
+
+import org.springframework.transaction.annotation.Transactional;
+
+import java.time.LocalDateTime;
+
+import java.util.UUID;
+
+@Service
+@RequiredArgsConstructor
+public class InventoryService {
+
+    private final InventoryRepository inventoryRepository;
+
+    private final InventoryMapper inventoryMapper;
+
+    @Transactional
+    public InventoryResponse reserveStock(
+            UUID productId,
+            Integer quantity
+    ) {
+
+        Inventory inventory =
+                inventoryRepository
+                        .findByProductId(productId)
+                        .orElseThrow(() ->
+                                new ResourceNotFoundException(
+                                        "Inventory not found"
+                                )
+                        );
+
+        if (inventory.getAvailableStock() < quantity) {
+
+            throw new IllegalArgumentException(
+                    "Insufficient stock available"
+            );
+        }
+
+        inventory.setAvailableStock(
+                inventory.getAvailableStock()
+                        - quantity
+        );
+
+        inventory.setReservedStock(
+                inventory.getReservedStock()
+                        + quantity
+        );
+
+        inventory.setUpdatedAt(
+                LocalDateTime.now()
+        );
+
+        Inventory savedInventory =
+                inventoryRepository.save(
+                        inventory
+                );
+
+        return inventoryMapper.toResponse(
+                savedInventory
+        );
+    }
+}
