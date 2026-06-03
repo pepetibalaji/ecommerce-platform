b638df13ef1db00ca0785c3d8c12cc05662f2c87
PEPETI BALAJI <pepeti.balaji@oracle.com>
2026-06-03 23:40:56 +0530

    feat(inventory): add inventory domain model

--- inventory-service/src/main/java/com/ecommerce/inventory/InventoryServiceApplication.java
+++ inventory-service/src/main/java/com/ecommerce/inventory/InventoryServiceApplication.java
@@ -1,3 +1,8 @@
+package com.ecommerce.inventory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 @SpringBootApplication
 public class InventoryServiceApplication {
 
---
+++ inventory-service/src/main/java/com/ecommerce/inventory/repository/InventoryRepository.java
@@ -0,0 +1,14 @@
+package com.ecommerce.inventory.repository;
+
+import com.ecommerce.inventory.entity.Inventory;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+import java.util.UUID;
+
+public interface InventoryRepository
+        extends JpaRepository<Inventory, UUID> {
+
+    Optional<Inventory> findByProductId(UUID productId);
+}
